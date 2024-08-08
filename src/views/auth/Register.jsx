7bf14f96a50f434276399be4eba5dcec923599a5
Harry MacCode <emarh.harry.code@gmail.com>
2024-08-08 14:29:21 +0200

    Route and Login Regsiter Part 4

--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Register = () => {
   return (
@@ -13,6 +14,29 @@ const Register = () => {
                     <label htmlFor="name">Name</label>
                     <input type="text" className='px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md' name='name' 
                         placeholder='Enter your name' id='name' required />
+                </div>
+
+                <div className='flex flex-col w-full gap-1 mb-3'>
+                    <label htmlFor="email">Email</label>
+                    <input type="text" className='px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md' name='email' 
+                        placeholder='Enter your email' id='email' required />
+                </div>
+
+                <div className='flex flex-col w-full gap-1 mb-3'>
+                    <label htmlFor="pasword">Password</label>
+                    <input type="password" className='px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md' name='password' 
+                        placeholder='Enter your password' id='password' required />
+                </div>
+
+                <div className='flex items-center w-full gap-3 mb-3'>
+                    <input type='checkbox' className='w-4 h-4 text-blue-600 overflow-hidden bg-gray-200 rounded border-gray-300 focus:ring-blue-500' name='checkbox'/>
+                    <label htmlFor="checkbox">I agree to privacy policy & treams</label>
+                </div>
+
+                <button className='bg-slate-800 w-full hover:shadow-blue-300 hover:shadow-lg text-white rounded-md px-7 py-2 mb-3'>Sign Up</button>
+
+                <div className='flex items-center mb-3 gap-3 justify-center'>
+                    <p>Already Have an account ? <Link className='font-bold' to='/login'>Sign in</Link></p>
                 </div>
             </form>
         </div>
