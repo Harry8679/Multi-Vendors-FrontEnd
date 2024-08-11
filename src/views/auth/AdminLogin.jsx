3e86e4394d7fe92013c7da9d5024b1027c2605eb
Harry MacCode <emarh.harry.code@gmail.com>
2024-08-11 13:37:54 +0200

    .gitignore file updated

--- src/views/auth/AdminLogin.jsx
+++ src/views/auth/AdminLogin.jsx
@@ -25,6 +25,11 @@ const AdminLogin = () => {
     <div className='min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center'>
       <div className='w-[350px] text-[#fff] p-2'>
         <div className='bg-[#6f68d1] p-4 rounded-md'>
+            <div>
+                <div>
+                    <img src={`${process.env.PUBLIC_URL}/images/logo.png`} alt="Logo" />
+                </div>
+            </div>
             <form action="" onSubmit={handleSubmit}>
                 <div className='flex flex-col w-full gap-1 mb-3'>
                     <label htmlFor="email">Email</label>
